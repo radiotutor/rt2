55b0bd9bed977af111cd7ff3f7fc1e81f01afc95
Peter <pe5erb@gmail.com>
2020-06-02 13:53:49 +0000

    fixed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,19 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404m Band: NOT FOUND</h1>
-    <p>It doesn't look like there's anyone active on the 404m band.</p>
-  </Layout>
-)
+const browser = typeof window !== "undefined" && window;
+
+const NotFoundPage = () => {
+  return (
+    browser && (
+      <Layout>
+        <SEO title="404: Not found" />
+        <h1>404m Band: NOT FOUND</h1>
+        <p>It doesn't look like there's anyone active on the 404m band.</p>
+      </Layout>
+    )
+  )
+}
 
 export default NotFoundPage
--- src/pages/course/foundation.js
+++ src/pages/course/foundation.js
@@ -6,9 +6,12 @@ import SEO from "../../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="Foundation Course" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>These resources will help you to work towards getting your Foundation licence, and your first call-sign!</p>
+    <p>We are working on our own Foundation course here at RadioTutor, so watch this space!</p>
+    <a href="https://www.essexham.co.uk/train/foundation-slides/">Essex Ham</a>
+    <a href="http://www.g0mwt.org.uk/training/courses/foundation.htm">Chelmsford Amateur Radio Society</a>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
